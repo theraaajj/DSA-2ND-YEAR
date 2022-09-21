942014a7409619e0387695367af4f6cff5cbb954
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2022-09-21 18:54:43 +0530

    Create QUESTION-3.CPP

---
+++ ASSIGNMENT  1/QUESTION-3.CPP
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a ;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    cout<< a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<setprecision(20)<<d<<"\n";
+    cout<<setprecision(20)<<e<<"\n";
+    return 0;
+}
