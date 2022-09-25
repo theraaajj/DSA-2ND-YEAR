9ab2b2d5af20d87a53f9ea37be20fefca7493121
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2022-09-25 23:21:23 +0530

    Create QUESTION 7.C

---
+++ ASSIGNMENT 2/QUESTION 7.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    int n;
+scanf("%d", &n);
+int digit, sum = 0;
+
+while (n>0) {
+        sum += (n%10);
+        n=n/10;
+    }
+printf("%d\n",sum);
+return 0;
+}
