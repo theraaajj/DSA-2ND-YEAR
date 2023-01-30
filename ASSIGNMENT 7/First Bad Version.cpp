3ce1e4d536d9284ec2d1befb44d46d4249d7b078
RAJ ARYAN <114001897+theraaajj@users.noreply.github.com>
2023-01-30 22:56:41 +0530

    Create First Bad Version.cpp

---
+++ ASSIGNMENT 7/First Bad Version.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long firstBadVersion(int n) {
+        long h=n, l=1;
+        while(l<=h){
+            long mid=(l+h)/2;
+            if(isBadVersion(mid)){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return l;
+
+    }
+};
